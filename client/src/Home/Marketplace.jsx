8cc31c0092952e78460521d14caaeee571ddea08
sanjai.me21 <sanjai.me21@bitsathy.ac.in>
2025-01-30 09:58:44 +0530

    first commit

--- client/src/Home/Marketplace.jsx
+++ client/src/Home/Marketplace.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import Database from "../data/Database";
 const Marketplace = () => {
   return (
-    <div>Marketplace</div>
-  )
-}
+    <div className=" grid grid-cols-4 gap-5 px-10">
+      {Database &&
+        Database?.map((user, index) => (
+          <div key={index}>
+            <h2>{user.bussinessName}</h2>
+            <p>{user.businessSector}</p>
+          </div>
+        ))}
+    </div>
+  );
+};
 
-export default Marketplace
+export default Marketplace;
