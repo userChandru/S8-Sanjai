181d9f42b5e909d2fdbd9874afdf7e0608030df2
userChandru <chandrugayathri227@gmail.com>
2025-02-27 11:09:53 +0530

    server side authentication updation

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/database');
 const validateSchemas = require('./utils/schemaCheck');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,6 +32,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/businesses', require('./routes/businessRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/inventories', require('./routes/inventoryRoutes'));
+app.use('/api/auth', authRoutes);
 console.log('Routes registered:', app._router.stack.map(r => r.route?.path).filter(Boolean));
 
 // Error handling middleware
