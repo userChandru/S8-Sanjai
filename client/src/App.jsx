30d361d537e1bcf0b8ae0177f5b2fb7d63337a24
userChandru <chandrugayathri227@gmail.com>
2025-03-20 12:59:23 +0530

    resolved admin login error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,62 +17,65 @@ import { CartProvider } from "./context/CartContext";
 import { Toaster } from "react-hot-toast";
 import BusinessPage from './Pages/BusinessPage';
 import PurchasePage from './Pages/PurchasePage';
+import { UserProvider } from "./context/UserContext";
 
 const App = () => {
   return (
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <CartProvider>
-        <div className="font-normal text-gray-800 bg-gray-100 min-h-screen max-w-screen *:min-h-screen *:max-w-screen">
-          <Toaster
-            position="top-right"
-            toastOptions={{
-              success: {
-                style: {
-                  background: "#10B981",
-                  color: "white",
+    <UserProvider>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <CartProvider>
+          <div className="font-normal text-gray-800 bg-gray-100 min-h-screen max-w-screen *:min-h-screen *:max-w-screen">
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                success: {
+                  style: {
+                    background: "#10B981",
+                    color: "white",
+                  },
+                  duration: 3000,
                 },
-                duration: 3000,
-              },
-              error: {
-                style: {
-                  background: "#EF4444",
-                  color: "white",
+                error: {
+                  style: {
+                    background: "#EF4444",
+                    color: "white",
+                  },
+                  duration: 3000,
                 },
-                duration: 3000,
-              },
-            }}
-          />
-          <BrowserRouter>
-            <Routes>
-              <Route index element={<AuthPage />} />
-              <Route path="/auth" element={<Authentication />} />
+              }}
+            />
+            <BrowserRouter>
+              <Routes>
+                <Route index element={<AuthPage />} />
+                <Route path="/auth" element={<Authentication />} />
 
-              {/* Student Routes */}
-              <Route element={<HomePage />}>
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/marketplace" element={<Marketplace />} />
-                <Route path="/bank" element={<Bank />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/edit-product" element={<EditProduct />} />
-                <Route path="/name" element={<Product />} />
-              </Route>
+                {/* Student Routes */}
+                <Route element={<HomePage />}>
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/marketplace" element={<Marketplace />} />
+                  <Route path="/bank" element={<Bank />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/edit-product" element={<EditProduct />} />
+                  <Route path="/name" element={<Product />} />
+                </Route>
 
-              {/* Admin Routes */}
-              <Route element={<AdminLayout />}>
-                <Route path="/admin/dashboard" element={<AdminDashboard />} />
-                <Route path="/admin/bank" element={<AdminBank />} />
-              </Route>
+                {/* Admin Routes */}
+                <Route element={<AdminLayout />}>
+                  <Route path="/admin/dashboard" element={<AdminDashboard />} />
+                  <Route path="/admin/bank" element={<AdminBank />} />
+                </Route>
 
-              <Route path="/business" element={<BusinessPage />} />
+                <Route path="/business" element={<BusinessPage />} />
 
-              <Route path="/purchase" element={<PurchasePage />} />
+                <Route path="/purchase" element={<PurchasePage />} />
 
-              <Route path="*" element={<ErrorPage />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
-      </CartProvider>
-    </GoogleOAuthProvider>
+                <Route path="*" element={<ErrorPage />} />
+              </Routes>
+            </BrowserRouter>
+          </div>
+        </CartProvider>
+      </GoogleOAuthProvider>
+    </UserProvider>
   );
 };
 
